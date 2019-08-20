51ca55d3d22f8adf72d0d17cc6d9e85d26db60da
Iyke <iykedev@gmail.com>
2019-08-20 21:06:00 +0100

    change port

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.use('/api/fixture', fixtureRoutes);
 
 mongoose.connect(process.env.DB_CONNECTION_STRING,{ useNewUrlParser: true }, ()=> console.log('connected'))
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port);
